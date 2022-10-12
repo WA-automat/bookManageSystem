4bb3098cc3b16cb1765a21289921a02a17950eae
WA_automat <1577696824@qq.com>
2022-10-12 10:00:50 +0800

    mybatisplus config

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -2,9 +2,10 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // 开启自动缓存
 @EnableCaching
 public class BackendApplication {
---
+++ src/main/java/com/example/backend/config/MyBatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.example.backend.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan({"com.example.backend.mapper", "com.example.backend.pojo"})
+public class MyBatisPlusConfig {
+	/**
+	 * 分页
+	 */
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+		PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+		mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
+		return mybatisPlusInterceptor;
+	}
+}
