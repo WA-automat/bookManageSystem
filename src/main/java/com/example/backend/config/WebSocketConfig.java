7632a22aed04372a154464f06672955b48da4bcf
WA_automat <1577696824@qq.com>
2022-10-12 16:40:27 +0800

    websocket

--- src/main/java/com/example/backend/config/WebSocketConfig.java
+++ src/main/java/com/example/backend/config/WebSocketConfig.java
@@ -4,6 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+/**
+ * @author WA_automat
+ * @version 1.0
+ * @since 2022-10-12
+ */
+// 后续实现websocket相关内容
+// (继续画饼)
 @Configuration
 public class WebSocketConfig {
 
